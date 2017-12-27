19bef1a3251a20ac98883390f8a82653ad5ce73f
fatemaislam <islamfatema04@gmail.com>
2017-12-26 19:59:19 -0500

    Cross Broser test

---
+++ src/test/java/crossbrowser/XmlDemo.java
@@ -0,0 +1,11 @@
+package crossbrowser;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class XmlDemo {
+@Test
+public void displayResult() {
+	System.out.println("Welcome to TestNG");
+}
+}
---
+++ src/test/java/crossbrowser/XmlDemo1.java
@@ -0,0 +1,11 @@
+package crossbrowser;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class XmlDemo1 {
+@Test
+public void displayResult() {
+	System.out.println("Welcome to Selenium World");
+}
+}
